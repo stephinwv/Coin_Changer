95439a88881fd7f1cc938917ba2b12d9f5cdc295
stephinwv <thekingdomseeker@aol.com>
2017-07-22 16:00:54 -0400

    Add files via upload
    Added def for nickel, and test for nickel. Both pass

--- coin.rb
+++ coin.rb
@@ -2,5 +2,8 @@ def coin(change)
 	coin_value = {quarter: 25, dime: 10, nickel: 5, penny: 1}
 	if change == 1
 		{quarter: 0, dime:0, nickel: 0, penny: 1}
-	end
+	
+	 else change == 5
+        {quater: 0, dime: 0, nickel: 1, penny: 0}
+    end
 end
--- test_coin.rb
+++ test_coin.rb
@@ -7,7 +7,10 @@ class TestCoinChanger < Minitest::Test
 		assert_equal(1,1)
 	end
 	
- 	def test_one_cent
+ 	def test_for_penny
     	assert_equal({quarter: 0, dime: 0, nickel: 0, penny: 1}, coin(1))
   	end  
+  	 def test_for_nickel 
+        assert_equal({quater: 0, dime: 0, nickel: 1, penny: 0}, coin(5))
+    end
 end
