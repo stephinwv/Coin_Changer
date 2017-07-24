3f797158383290055199e98b8d4236730a1342ef
stephinwv <thekingdomseeker@aol.com>
2017-07-24 19:38:40 -0400

    Add files via upload
    All tests pass

--- coin.rb
+++ coin.rb
@@ -1,17 +1,14 @@
-def coin(change)
-	coin_value = {quarter: 25, dime: 10, nickel: 5, penny: 1}
-	if change == 1
-		{quarter: 0, dime:0, nickel: 0, penny: 1}
-	
-		elsif change == 5
-        {quater: 0, dime: 0, nickel: 1, penny: 0}
-    
-    	elsif change == 10
-    	{quarter: 0, dime: 1, nickel: 0, penny: 0}
-    
-    	else change == 25
-    	{quarter: 1, dime: 0, nickel: 0, penny: 0}
-
-    end
-
+def coin_changer(amount)
+ start_values={quarter:0, dime:0, nickel:0, penny:0}
+ coin_choices= {quarter:25, dime:10, nickel:5, penny:1}
+ 
+ coin_choices.each do |coin, value|
+    while
+    value<=amount
+    start_values[coin]+=1
+    amount=amount-value
+   end 
 end
+
+start_values
+ end
--- test_coin.rb
+++ test_coin.rb
@@ -1,22 +1,55 @@
 require "minitest/autorun"
 require_relative "coin.rb"
 
-class TestCoinChanger < Minitest::Test
 
-    def test_1_equal_1_test
+class TestCoin < Minitest::Test
+	def test_one_to_one
 		assert_equal(1,1)
+ 	end
+ 	def test_for_one_cent
+		assert_equal({quarter:0, dime:0, nickel:0, penny:1},coin_changer(1))
 	end
-	
- 	def test_for_penny
-    	assert_equal({quarter: 0, dime: 0, nickel: 0, penny: 1}, coin(1))
-  	end  
-  	 def test_for_nickel 
-        assert_equal({quater: 0, dime: 0, nickel: 1, penny: 0}, coin(5))
-    end
-    def test_for_dime
-    	assert_equal({quarter: 0, dime: 1, nickel: 0, penny: 0}, coin(10))
-    end
-    def test_for_quarter
-    	assert_equal({quarter: 1, dime: 0, nickel: 0, penny: 0}, coin(25))
-    end
+
+	def test_for_five_cents
+		assert_equal({quarter:0, dime:0, nickel:1, penny:0},coin_changer(5))
+	end
+
+	def test_for_ten_cents
+		assert_equal({quarter:0, dime:1, nickel:0, penny:0},coin_changer(10))
+	end
+
+	def test_for_twenty_five_cents
+		assert_equal({quarter:1, dime:0, nickel:0, penny:0},coin_changer(25))
+	end
+
+ 	def test_for_six_cents
+ 		assert_equal({quarter:0, dime:0, nickel:1, penny:1},coin_changer(6))
+ 	end
+ 
+ 	def test_for_two_cents
+ 		assert_equal({quarter:0, dime:0, nickel:0, penny:2},coin_changer(2))
+ 	end
+ 	def test_for_fifty_five_cents
+ 		assert_equal({quarter:2, dime:0, nickel:1, penny:0},coin_changer(55))
+ 	end
+ 
+ 	def test_for_sixteen_cents
+ 		assert_equal({quarter:0, dime:1, nickel:1, penny:1},coin_changer(16))
+ 	end
+ 
+ 	def test_for_three_cents
+ 		assert_equal({quarter:0, dime:0, nickel:0, penny:3},coin_changer(3))
+ 	end
+
+	def test_for_ninety_nine_cents
+		assert_equal({quarter:3, dime:2, nickel:0, penny:4},coin_changer(99))
+	end	
 end
+
+
+
+
+
+
+
+		
